4af473d80bb4f741c413e5ebc7462173d4889d03
Andrew Loeliger <aloelige@cern.ch>
2019-08-14 17:21:25 -0400

    Updated moving script for forgotten distributions

--- MoveAll2016.sh
+++ MoveAll2016.sh
@@ -15,11 +15,14 @@ mv SMHTT2016SelectionConfigs/VBF_0.root $1/VBF.root
 mv SMHTT2016SelectionConfigs/WHMinus_0.root $1/WHMinus.root
 mv SMHTT2016SelectionConfigs/WHPlus_0.root $1/WHPlus.root
 mv SMHTT2016SelectionConfigs/WW1L1Nu2Q_0.root $1/WW1L1Nu2Q.root
+mv SMHTT2016SelectionConfigs/WZ1L1Nu2Q_0.root $1/WZ1L1Nu2Q.root
 mv SMHTT2016SelectionConfigs/WZ2L2Q_0.root $1/WZ2L2Q.root
+mv SMHTT2016SelectionConfigs/WZ1L3Nu_0.root $1/WZ1L3Nu.root
 mv SMHTT2016SelectionConfigs/WZJLLLNu_0.root $1/WZJLLLNu.root
 hadd SMHTT2016SelectionConfigs/W.root SMHTT2016SelectionConfigs/W_?.root
 rm SMHTT2016SelectionConfigs/W_?.root
 mv SMHTT2016SelectionConfigs/W.root $1/W.root
 mv SMHTT2016SelectionConfigs/ZH_0.root $1/ZH.root
+mv SMHTT2016SelectionConfigs/ZZ2L2Q_0.root $1/ZZ2L2Q.root
 mv SMHTT2016SelectionConfigs/ZZ4L_0.root $1/ZZ4L.root
 mv SMHTT2016SelectionConfigs/ggH_0.root $1/ggH.root
