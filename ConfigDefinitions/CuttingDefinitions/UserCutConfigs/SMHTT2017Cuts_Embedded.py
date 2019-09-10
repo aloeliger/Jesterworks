43b4c90e2e25ee5674f87eecaab4b91fdba3a6f2
Andrew Loeliger <aloelige@cern.ch>
2019-09-10 06:57:58 -0400

    SMHTT 2017 Cutting defintions

---
+++ ConfigDefinitions/CuttingDefinitions/UserCutConfigs/SMHTT2017Cuts_Embedded.py
@@ -0,0 +1,14 @@
+import ConfigDefinitions.CuttingDefinitions.CutDef as CutDef
+
+SMHTT2017Cuts = CutDef.UserCutConfig()
+SMHTT2017Cuts.Cuts=[]
+SMHTT2017Cuts.Cuts.append("abs(eta_1) > 2.1 && abs(eta_2) > 2.3")
+SMHTT2017Cuts.Cuts.append("Flag_goodVertices && Flag_globalSuperTightHalo2016Filter && Flag_HBHENoiseFilter && Flag_HBHENoiseIsoFilter && Flag_EcalDeadCellTriggerPrimitiveFilter && Flag_BadPFMuonFilter && Flag_BadChargedCandidateFilter && Flag_eeBadScFilter && Flag_ecalBadCalibFilter")
+SMHTT2017Cuts.Cuts.append("Trigger24 || Trigger27 || Trigger2027")
+SMHTT2017Cuts.Cuts.append("againstElectronVLooseMVA6_2 && againstMuonTight3_2")
+SMHTT2017Cuts.Cuts.append("nbtag==0")
+SMHTT2017Cuts.Cuts.append("pt_2 > 20")
+#SMHTT2017Cuts.Cuts.append("!(gen_match_1 > 2 && gen_match_1 < 6 && gen_match_2 > 2 && gen_match_2 < 6)")
+#SMHTT2017Cuts.Cuts.append("gen_match_2 != 6")
+SMHTT2017Cuts.Cuts.append("q_1 * q_2 < 0")
+SMHTT2017Cuts.Cuts.append("byTightIsolationMVArun2v2DBoldDMwLT_2 && iso_1 < 0.15")
