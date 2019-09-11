3d3ef51d657e9de03064197a3efb4c6b30b36b5d
Andrew Loeliger <aloelige@cern.ch>
2019-09-11 12:19:45 -0400

    test 2016 config

---
+++ ConfigDefinitions/UserConfigs/SMHTT_2016_Configs/DYConfig.py
@@ -0,0 +1,8 @@
+from ConfigDefintions.JesterworksConfigurations import JesterworksConfiguration as Config
+
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2016_Collection import SMHTT_2016_Collection as BranchCollection
+from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2016Cuts import SMHTT2016Cuts as CutConfig
+from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
+
+DataConfig = Config()
+
