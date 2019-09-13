b8003c946357e7088b39a1b024cab08f2df8cfe6
Andrew Loeliger <aloelige@cern.ch>
2019-09-13 17:33:52 -0400

    Created all 2018 configs

---
+++ ConfigDefinitions/UserConfigs/SMHTT_2018_AntiIso_Config/WZConfig.py
@@ -0,0 +1,17 @@
+from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
+
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2018_MC_Collection import MCCollection as BranchCollection
+from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2018Cuts_MC_NoEmbeddedOverlap_AntiIso import SMHTT2018Cuts as CutConfig
+from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
+
+DataConfig = Config()
+DataConfig.Path = "/data/ccaillol/smhmt2018_svfitted_4aug/"
+DataConfig.Files = ["WZ.root"]
+DataConfig.InputTreeName = "mutau_tree"
+DataConfig.SampleName = "WZ"
+DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2018_AntiIso/"
+DataConfig.OutputFile = "WZ.root"
+DataConfig.OutputTreeName = "mt_Selected"
+DataConfig.BranchCollection = BranchCollection
+DataConfig.CutConfig = CutConfig
+DataConfig.EndAction = HistogramGrabber
