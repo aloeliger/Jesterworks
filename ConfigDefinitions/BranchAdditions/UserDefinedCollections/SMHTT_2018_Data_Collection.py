efd7b22a115818b1d20a13a8ff93060364f3a708
Andrew Loeliger <aloelige@cern.ch>
2019-09-13 17:30:27 -0400

    Created 2018 branch collections

---
+++ ConfigDefinitions/BranchAdditions/UserDefinedCollections/SMHTT_2018_Data_Collection.py
@@ -0,0 +1,7 @@
+import ConfigDefinitions.BranchAdditions.BranchDef as BranchDef
+import ConfigDefinitions.BranchAdditions.UserDefinedBranches.Triggers_18_Data as Triggers
+
+DataCollection = BranchDef.UserBranchCollection()
+DataCollection.UserBranches=[Triggers.Trigger24,
+                             Triggers.Trigger27,
+                             Triggers.Trigger2027]
---
+++ ConfigDefinitions/BranchAdditions/UserDefinedCollections/SMHTT_2018_Embedded_Collection.py
@@ -0,0 +1,7 @@
+import ConfigDefinitions.BranchAdditions.BranchDef as BranchDef
+import ConfigDefinitions.BranchAdditions.UserDefinedBranches.Triggers_18_Embedded as Triggers
+
+EmbeddedCollection = BranchDef.UserBranchCollection()
+EmbeddedCollection.UserBranches=[Triggers.Trigger24,
+                                 Triggers.Trigger27,
+                                 Triggers.Trigger2027]
