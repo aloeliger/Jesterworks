3d7c3a275a4a05ab0270bde638a0d20dd281ef31
Andrew Loeliger <aloelige@cern.ch>
2019-09-13 17:33:16 -0400

    Finished all 2016 configs

---
+++ ConfigDefinitions/UserConfigs/SMHTT_2016_AntiIso_Configs/ST_tW_topConfig.py
@@ -0,0 +1,17 @@
+from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
+
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2016_Collection import SMHTT_2016_Collection as BranchCollection
+from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2016Cuts_AntiIso import SMHTT2016Cuts as CutConfig
+from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
+
+DataConfig = Config()
+DataConfig.Path = "/data/ccaillol/smhmt2016_svfitted_16aug/"
+DataConfig.Files = ["ST_tW_top.root"]
+DataConfig.InputTreeName = "mutau_tree"
+DataConfig.SampleName = "ST_tW_top"
+DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2016_AntiIso/"
+DataConfig.OutputFile = "ST_tW_top.root"
+DataConfig.OutputTreeName = "mt_Selected"
+DataConfig.BranchCollection = BranchCollection
+DataConfig.CutConfig = CutConfig
+DataConfig.EndAction = HistogramGrabber
---
+++ ConfigDefinitions/UserConfigs/SMHTT_2016_Configs/WHMinusConfig.py
@@ -0,0 +1,17 @@
+from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
+
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2016_Collection import SMHTT_2016_Collection as BranchCollection
+from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2016Cuts import SMHTT2016Cuts as CutConfig
+from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
+
+DataConfig = Config()
+DataConfig.Path = "/data/ccaillol/smhmt2016_svfitted_16aug/"
+DataConfig.Files = ["WminusH125.root"]
+DataConfig.InputTreeName = "mutau_tree"
+DataConfig.SampleName = "WHMinus"
+DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2016/"
+DataConfig.OutputFile = "WHMinus.root"
+DataConfig.OutputTreeName = "mt_Selected"
+DataConfig.BranchCollection = BranchCollection
+DataConfig.CutConfig = CutConfig
+DataConfig.EndAction = HistogramGrabber
