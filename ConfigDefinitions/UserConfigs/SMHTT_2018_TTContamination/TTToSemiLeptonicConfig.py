ca8c8c471534abdd73107c1603e8af3a2786ab33
Andrew Loeliger <aloelige@cern.ch>
2019-09-23 11:43:22 -0400

    Added in TTContamination configuration

---
+++ ConfigDefinitions/UserConfigs/SMHTT_2018_TTContamination/TTToSemiLeptonicConfig.py
@@ -0,0 +1,17 @@
+from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
+
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2018_MC_Collection import MCCollection as BranchCollection
+from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2018Cuts_TTContamination import SMHTT2018Cuts as CutConfig
+from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
+
+DataConfig = Config()
+DataConfig.Path = "/data/ccaillol/smhmt2018_svfitted_4aug/"
+DataConfig.Files = ["TTToSemiLeptonic.root"]
+DataConfig.InputTreeName = "mutau_tree"
+DataConfig.SampleName = "TTToSemiLeptonic"
+DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2018_TTContamination/"
+DataConfig.OutputFile = "TTToSemiLeptonic.root"
+DataConfig.OutputTreeName = "mt_Selected"
+DataConfig.BranchCollection = BranchCollection
+DataConfig.CutConfig = CutConfig
+DataConfig.EndAction = HistogramGrabber
