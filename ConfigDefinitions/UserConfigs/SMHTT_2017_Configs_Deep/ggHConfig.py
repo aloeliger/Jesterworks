a4eb90900c7d0e51c1ab298189106c3e8db476aa
Andrew Loeliger <aloelige@cern.ch>
2019-10-22 10:30:23 -0400

    Add proper 2017 Deep configurations

---
+++ ConfigDefinitions/UserConfigs/SMHTT_2017_Configs_Deep/ggHConfig.py
@@ -0,0 +1,17 @@
+from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
+
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2017_MC_Data_Collection import MC_Data_Collection as BranchCollection
+from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2017Cuts_MC_Data_wDeep import SMHTT2017Cuts as CutConfig
+from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
+
+DataConfig = Config()
+DataConfig.Path = "/data/ccaillol/smhmt2017_svfitted_12oct/"
+DataConfig.Files = ["ggH125.root"]
+DataConfig.InputTreeName = "mutau_tree"
+DataConfig.SampleName = "ggH"
+DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2017_Deep/"
+DataConfig.OutputFile = "ggH.root"
+DataConfig.OutputTreeName = 'mt_Selected'
+DataConfig.BranchCollection = BranchCollection
+DataConfig.CutConfig = CutConfig
+DataConfig.EndAction = HistogramGrabber
