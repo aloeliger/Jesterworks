04fa3ab0b906ad5bb706f23d8417cc5f6c4fb924
Andrew Loeliger <aloelige@cern.ch>
2019-12-02 07:15:42 -0500

    Fixed configurations

--- ConfigDefinitions/UserConfigs/SMHTT_2018_Configs_Deep/TTToHadronicConfig.py
+++ ConfigDefinitions/UserConfigs/SMHTT_2018_Configs_Deep/TTToHadronicConfig.py
@@ -1,6 +1,7 @@
 from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
 
 from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2018_MC_Collection import MCCollection as BranchCollection
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.EScaleCollection_2018 import EScaleCollection  as PostfixCollection
 from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2018Cuts_MC_NoEmbeddedOverlap_wDeep import SMHTT2018Cuts as CutConfig
 from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
 
