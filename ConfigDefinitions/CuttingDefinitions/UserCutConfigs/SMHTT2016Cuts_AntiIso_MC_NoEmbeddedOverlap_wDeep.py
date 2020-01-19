25f47b56370b218fe62c9a76cc51caab09a7a8b5
Andrew Loeliger <aloelige@cern.ch>
2020-01-19 07:38:16 -0500

    Fixed anti-isolation selection neglecting jets necessary for fakes

--- ConfigDefinitions/CuttingDefinitions/UserCutConfigs/SMHTT2016Cuts_AntiIso_MC_NoEmbeddedOverlap_wDeep.py
+++ ConfigDefinitions/CuttingDefinitions/UserCutConfigs/SMHTT2016Cuts_AntiIso_MC_NoEmbeddedOverlap_wDeep.py
@@ -7,7 +7,7 @@ SMHTT2016Cuts.Cuts.append("abs(eta_1)<2.1 && abs(eta_2)<2.3")
 SMHTT2016Cuts.Cuts.append("pt_1 > 20.0 && pt_2 > 20.0")
 SMHTT2016Cuts.Cuts.append("!(Flag_goodVertices || Flag_globalSuperTightHalo2016Filter || Flag_HBHENoiseIsoFilter || Flag_HBHENoiseFilter || Flag_EcalDeadCellTriggerPrimitiveFilter || Flag_BadPFMuonFilter)")
 SMHTT2016Cuts.Cuts.append("Trigger22 || Trigger1920")
-SMHTT2016Cuts.Cuts.append("gen_match_2 != 6")#avoid overlap with FF's
+#SMHTT2016Cuts.Cuts.append("gen_match_2 != 6")#avoid overlap with FF's
 SMHTT2016Cuts.Cuts.append("!(gen_match_1 > 2 && gen_match_1 < 6 && gen_match_2 > 2 && gen_match_2 < 6)")
 SMHTT2016Cuts.Cuts.append("byVVVLooseDeepVSe_2 && byTightDeepVSmu_2")
 SMHTT2016Cuts.Cuts.append("DeltaR > 0.5")
--- ConfigDefinitions/CuttingDefinitions/UserCutConfigs/SMHTT2018Cuts_Embedded_AntiIso_wDeep.py
+++ ConfigDefinitions/CuttingDefinitions/UserCutConfigs/SMHTT2018Cuts_Embedded_AntiIso_wDeep.py
@@ -5,7 +5,7 @@ SMHTT2018Cuts.Cuts=[]
 SMHTT2018Cuts.Cuts.append("abs(eta_1)<2.1 && abs(eta_2) < 2.3")
 SMHTT2018Cuts.Cuts.append("!(Flag_goodVertices || Flag_globalSuperTightHalo2016Filter || Flag_HBHENoiseFilter || Flag_HBHENoiseIsoFilter || Flag_EcalDeadCellTriggerPrimitiveFilter || Flag_BadPFMuonFilter || Flag_eeBadScFilter || Flag_ecalBadCalibReducedMINIAODFilter)")
 SMHTT2018Cuts.Cuts.append("Trigger24 || Trigger27 || Trigger2027")
-SMHTT2018Cuts.Cuts.append("gen_match_2 != 6")#avoid overlap with FF's
+#SMHTT2018Cuts.Cuts.append("gen_match_2 != 6")#avoid overlap with FF's
 SMHTT2018Cuts.Cuts.append("byVVVLooseDeepVSe_2 && byTightDeepVSmu_2")
 SMHTT2018Cuts.Cuts.append("DeltaR > 0.5")
 SMHTT2018Cuts.Cuts.append("nbtag <= 0 && nbtagL <= 1")
