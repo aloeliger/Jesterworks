e4a12e91383627e315985ace8898686431c4b63b
Andrew Loeliger <aloelige@cern.ch>
2020-05-27 19:08:20 -0400

    QCD acceptance uncertainties

--- ConfigDefinitions/UserConfigs/SMHTT_2017_MCOnly_Configs_Deep/GGZHLLTTConfig.py
+++ ConfigDefinitions/UserConfigs/SMHTT_2017_MCOnly_Configs_Deep/GGZHLLTTConfig.py
@@ -5,7 +5,7 @@ from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2017Cuts_MC_wDeep 
 from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
 
 DataConfig = Config()
-DataConfig.Path = "/data/ccaillol/differential_mt2017_svfitted/"
+DataConfig.Path = "/data/ccaillol/smhmt2017_svfitted_11may/"
 DataConfig.Files = ["GGZHLLTT.root"]
 DataConfig.InputTreeName = "mutau_tree"
 DataConfig.SampleName = "ZH"
