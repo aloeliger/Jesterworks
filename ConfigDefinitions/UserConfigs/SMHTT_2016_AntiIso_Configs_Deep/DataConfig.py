7a7ec43e724ce8d480521ae32b4d3d2770b7c11d
Andrew Loeliger <aloelige@cern.ch>
2020-07-13 06:31:40 -0400

    Misc fixes

--- ConfigDefinitions/UserConfigs/SMHTT_2016_AntiIso_Configs_Deep/DataConfig.py
+++ ConfigDefinitions/UserConfigs/SMHTT_2016_AntiIso_Configs_Deep/DataConfig.py
@@ -1,6 +1,7 @@
 from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as Config
 
 from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2016_Collection import SMHTT_2016_Collection as BranchCollection
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.EScaleCollection_2016 import EScaleCollection as PostfixCollection
 from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2016Cuts_AntiIso_Data_wDeep import SMHTT2016Cuts as CutConfig
 from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
 
@@ -13,5 +14,6 @@ DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2016_AntiIso_Deep/"
 DataConfig.OutputFile = "Data.root"
 DataConfig.OutputTreeName = "mt_Selected"
 DataConfig.BranchCollection = BranchCollection
+DataConfig.PostfixBranchCollection = PostfixCollection
 DataConfig.CutConfig = CutConfig
 DataConfig.EndAction = HistogramGrabber
