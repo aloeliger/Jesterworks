8a63b86c82f9fb3f53c3f30fa6bc4765e0f5176a
Andrew Loeliger <loeliger@wisc.edu>
2020-09-13 08:53:19 -0500

    More work on the submission script

--- JesterworksUtils/submitJobsToCluster.py
+++ JesterworksUtils/submitJobsToCluster.py
@@ -8,16 +8,42 @@ from JesterworksUtils.RecursiveLoader
 from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration as JesterworksConfiguration
 import datetime
 import getpass    
+import subprocess
+import re
 
-def main():
-    #okay, we need to sort out what needs to happen here.
-    #what is necessary to attempt to run a combine job?
-    #a script, with our necessary command
+#create a submission file for use on condor
+#do it at the specified location.
+def createSubmissionFile(fileName):
+    #let's create the submission file at the specified location
+    #this should be an explicit creation of the file
+    theFile = open(fileName,'x')
+    #we need to make sure we know where our proxy is.
+    #this will have information specific to user, so we have no choice really
+    #except parsing some information and figuring it out ourself
+    vomsProxyInfoCommand = ['voms-proxy-info']
+    vomsProxyInfoProcess = subprocess.Popen(
+        vomsProxyInfoCommand,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    vomsProxyInfoOut, vomsProxyInfoErr = vomsProxyInfoProcess.communicate()
+    vomsProxyInfoExitcode = vomsProxyInfoProcess.wait()
+
+    if vomsProxyInfoExitcode <> 0:
+        raise RuntimeError(
+            'voms proxy info error: %s' % vomsProxyInfoOut+vomsProxyInfoErr
+            )
+    #okay, now let's find the location and name of the voms proxy
+    proxyFileLocation = re.search('(?:path\s+:\s)(/.*)*',vomsProxyInfoOut).group(1)
+    theFile.Write('X509UserProxy        = '+proxyFileLocation+'\n')
+    
+
+def main():    
     parser = argparse.ArgumentParser(description='Jesterworks submission tool for running configurations or parts of configurations on condor')
     parser.add_argument('--configFiles','-c',nargs='+',help='configurations to create submissions for',required=True)
     fileNumGroup = parser.add_mutually_exclusive_group(required=True)
     fileNumGroup.add_argument('--all',action='store_true',help='Run all files of the selected configurations')
-    fileNumGroup.add_argument('--onlyFile',nargs='+',help='Specify the numbers of files from the configuration to run.',type=int)
+    fileNumGroup.add_argument('--onlyFiles',nargs='+',help='Specify the numbers of files from the configuration to run.',type=int)
     
     #first things first, we should check whether there is a valid proxy.
     #if there isn't let's make one.
@@ -29,16 +55,31 @@ def main():
     #now that we have that, let's loop over each of our configurations
     theLoader = JesterworksUtils.RecursiveLoader.RecursiveLoader()
     for configFile in args.configFiles:
-        #let's quickly load the module so we have it available in case we need elements of it later. This can be gotten rid of if it is not useful.
+        #let's quickly load the module
         theConfigModule = theLoader.LoadFromDirectoryPath(configFile)
         for item in dir(theConfigModule):
             theConfig = getattr(theConfigModule,item)
             if isinstance(theConfig,JesterworksConfiguration):
                 break
         #okay, let's set up a submission directory if that doesn't exist
-        overallSubmissionDirectory = '/nfs_scratch/'+getpass.getuser()='/'+(configFile[configFile.rfind('/')+1:len(configFile)-3])
-        if not os.path.isdir(ocerallSubmissionDirectory):
+        configFileDirectoryName = configFile[configFile.rfind('/')+1:len(configFile)-3]+datetime/datetime.now().strftime('%d%m%y_%H%M')
+        overallSubmissionDirectory = '/nfs_scratch/'+getpass.getuser()='/'+configFileDirectoryName
+        if not os.path.isdir(overallSubmissionDirectory):
             os.mkdir(overallSubmissionDirectory)
+        #okay, at this point, let's consider whether we are doing this for all files of a configuration, or whether
+        # we are doing this only for specific files. 
+        indicesToProcess = []
+        if args.all:
+            indicesToProcess = range(0,len(theConfig.Files))
+        elif args.onlyFiles != None:
+            indicesToProcess = args.onlyFiles
+        for fileIndex in indicesToProcess:
+            #let's set up a directory specifically for whichever file it is we want to submit
+            fileNumSpecificName = overallSubmissionDirectory+'/'+configFileDirectoryName+''
+            if not os.path.isdir(overallSubmissionDirectory):
+                os.mkdir(fileNumSpecificName)
+                
+            #okay, our next job is to go ahead and make a submission file for this specific file we want to submit.
             
     
 
