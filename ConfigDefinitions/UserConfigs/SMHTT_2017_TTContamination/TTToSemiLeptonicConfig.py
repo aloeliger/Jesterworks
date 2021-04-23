96a22bac4ce20b91aba5884eb0e5667fcea3bc9a
Andrew David Loeliger <andrew.loeliger@cern.ch>
2021-04-23 07:25:06 -0400

    Update TTContamination to have proper postfix collections

--- ConfigDefinitions/UserConfigs/SMHTT_2017_TTContamination/TTToSemiLeptonicConfig.py
+++ ConfigDefinitions/UserConfigs/SMHTT_2017_TTContamination/TTToSemiLeptonicConfig.py
@@ -2,6 +2,7 @@ from ConfigDefinitions.JesterworksConfigurations import JesterworksConfiguration
 
 from ConfigDefinitions.BranchAdditions.UserDefinedCollections.SMHTT_2017_MC_Data_Collection import MC_Data_Collection as BranchCollection
 from ConfigDefinitions.CuttingDefinitions.UserCutConfigs.SMHTT2017Cuts_TTContamination import SMHTT2017Cuts as CutConfig
+from ConfigDefinitions.BranchAdditions.UserDefinedCollections.Differential_2017_Collection import DifferentialCollection as PostfixCollection
 from ConfigDefinitions.EndActionDefinitions.UserConfigs.GrabHistograms import HistogramGrabber as HistogramGrabber
 
 DataConfig = Config()
@@ -13,5 +14,6 @@ DataConfig.OutputPath = "/data/aloeliger/SMHTT_Selected_2017_TTContamination/"
 DataConfig.OutputFile = "TTToSemiLeptonic.root"
 DataConfig.OutputTreeName = 'mt_Selected'
 DataConfig.BranchCollection = BranchCollection
+DataConfig.PostfixBranchCollection = PostfixCollection
 DataConfig.CutConfig = CutConfig
 DataConfig.EndAction = HistogramGrabber
